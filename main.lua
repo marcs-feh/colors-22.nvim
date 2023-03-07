f18095951bc7e88ed4897b0152a97a6f47d031c1
marcs-feh <marcosfehlauer@protonmail.com>
2023-03-07 17:09:18 -0300

    add more integration

--- main.lua
+++ main.lua
@@ -6,17 +6,26 @@ local options = {
 	transparent = true,
 	italic_comments = false,
 	bright_cursor_line = false,
+
+	plugin_integrations = {
+		telescope = true,
+		mini_nvim = true,
+	},
 }
 
 local cursor_bg = options.bright_cursor_line and c.bg_alt or nil
 
+local tr_bg = c.bg -- Transparent bg
 if options.transparent then
-	c.bg = nil
+	tr_bg = nil
 end
 
+vim.cmd [[ colorscheme default ]]
+
+-- Neovim
 hi {
 	-- Core highlight groups
-	{'Normal', { bg = c.bg, fg = c.fg }},
+	{'Normal', { bg = tr_bg, fg = c.fg }},
 	{'Italic', { italic = true }},
 	{'Bold', { bold = true }},
 	{'Visual', { fg = c.fg_br, bg = c.bg_br }},
@@ -36,12 +45,21 @@ hi {
 	{'PmenuSBar', { bg = c.bg_br, fg = c.fg_br }},
 	{'PmenuThumb', { bg = c.fg_br, fg = c.bg_br }},
 	{'ColorColumn', { bg = c.bg_br }},
-	{'Folded', { bg = c.bg, fg = c.bg_br_alt, italic = not options.italic_comments }},
-	{'FoldedColumn', { bg = c.bg_br, fg = c.bg_br_alt, italic = not options.italic_comments }},
+	{'Folded', { bg = tr_bg, fg = c.bg_br_alt, italic = not options.italic_comments }},
+	{'FoldColumn', { bg = c.tr_bg, fg = c.bg_br_alt }},
 	{'Title', { fg = c.type, bold = true }},
+	{'NonText', { fg = c.type, bold = true }},
 	{'Tabline', { bg = c.bg_alt }},
 	{'TablineSel', { bg = c.bg_br, bold = true }},
-	{'TablineFill', { bg = c.bg }},
+	{'TablineFill', { bg = c.tr_bg }},
+	{'WinBar', { bg = c.bg_alt, fg = c.fg_alt }},
+	{'WinSeparator', { bg = c.bg, fg = c.bg }},
+	{'StatusLine', { bg = c.bg_alt, fg = c.fg_alt }},
+	{'StatusLineNC', { bg = c.bg, fg = c.bg_br_alt }},
+	{'DiffAdd', { fg = c.func }},
+	{'DiffText', { fg = c.reserved }},
+	{'DiffChange', { fg = c.reserved_alt }},
+	{'DiffDelete', { fg = c.bg_alt }},
 
 	-- Syntax
 	{'String', { fg = c.str }},
@@ -59,6 +77,10 @@ hi {
 	{'Conditional', { fg = c.reserved }},
 	{'PreProc', { fg = c.reserved_alt }},
 	{'Statement', { fg = c.fg_alt, bold = true }},
+	{'SpellBad', { fg = c.hint, undercurl = true }},
+	{'SpellCap', { fg = c.fg, undercurl = true }},
+	{'SpellLocal', { fg = c.fg, italic = true, undercurl = true }},
+	{'SpellRare', { fg = c.hint, undercurl = true }},
 
 	-- Diagnostic
 	{'DiagnosticError', { fg = c.error }},
@@ -70,9 +92,40 @@ hi {
 	{'DiagnosticUnderlineHint', { fg = c.hint, underline = true }},
 	{'DiagnosticUnderlineInfo', { fg = c.fg_alt, underline = true }},
 
-
 	-- Treesitter
 	{'@constant.builtin', { fg = c.literal_alt }},
 	{'@namespace', { fg = c.type_alt }},
+
 }
+
+-- Telescope
+if options.plugin_integrations.telescope then
+	hi {
+		{'TelescopeNormal', { bg = c.bg }},
+		{'TelescopePromptBorder', { fg = c.fg_alt, bg = c.bg }},
+		{'TelescopeResultsBorder', { fg = c.fg_alt, bg = c.bg }},
+		{'TelescopePreviewBorder', { fg = c.fg_alt, bg = c.bg }},
+		{'TelescopeSelection', { bg = c.bg_alt }},
+		{'TelescopeMultiSelection', { bg = c.bg_alt, bold = true }},
+		{'TelescopeMatching', { fg = c.literal_alt, bold = true }},
+		{'TelescopePromptPrefix', { fg = c.fg_alt, bg = c.bg }},
+	}
+end
+
+-- mini.nvim
+if options.plugin_integrations.mini_nvim then
+	hi {
+		{'MiniStatuslineFile', { bg = c.bg_br, fg = c.fg_alt }},
+		{'MiniStatuslineFileinfo', { bg = c.bg_br, fg = c.fg_alt }},
+		{'MiniStatuslineFilename', { bg = c.bg_alt, fg = c.fg }},
+		{'MiniStatuslineInactive', { bg = c.bg_alt, fg = c.bg_br_alt }},
+		{'MiniStatuslineDevinfo', { bg = c.bg_br, fg = c.fg_br }},
+		{'MiniStatuslineModeNormal', { bg = c.reserved, fg = c.bg, bold = true }},
+		{'MiniStatuslineModeInsert', { bg = c.type, fg = c.bg, bold = true }},
+		{'MiniStatuslineModeVisual', { bg = c.literal, fg = c.bg, bold = true }},
+		{'MiniStatuslineModeCommand', { bg = c.reserved_alt, fg = c.bg, bold = true }},
+		{'MiniStatuslineModeReplace', { bg = c.str_alt, fg = c.bg, bold = true }},
+		{'MiniStatuslineModeOther', { bg = c.bg_br_alt, fg = c.bg, bold = true }},
+	}
+end
 
