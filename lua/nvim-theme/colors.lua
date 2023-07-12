60652723ba7904fcdb2d75e2423b7a23e720a85a
marcs-feh <marcosfehlauer@protonmail.com>
2023-07-12 16:09:03 -0300

    add support for termguicolors

--- lua/nvim-theme/colors.lua
+++ lua/nvim-theme/colors.lua
@@ -1,33 +1,66 @@
 C = {
-	-- Main colors
-	bg        = '#1d2021',
-	bg_alt    = '#303536',
-	bg_br     = '#434a4c',
-	bg_br_alt = '#606a6c',
-	fg        = '#f4e5bf',
-	fg_alt    = '#eed89f',
-	fg_br     = '#fcf8ee',
-	fg_br_alt = '#f9f1dc',
+	regular = {
+		-- Main colors
+		bg        = '#1d2021',
+		bg_alt    = '#303536',
+		bg_br     = '#434a4c',
+		bg_br_alt = '#606a6c',
+		fg        = '#f4e5bf',
+		fg_alt    = '#eed89f',
+		fg_br     = '#fcf8ee',
+		fg_br_alt = '#f9f1dc',
 
-	-- Highlights
-	type         = '#81aece',
-	type_alt     = '#7ec9b1',
-	reserved     = '#cda2e3',
-	reserved_alt = '#af91d5',
-	id           = '#ebe3cb',
-	id_alt       = '#e6b995',
-	literal      = '#c4dc9b',
-	literal_alt  = '#8ac87d',
-	func         = '#ecdc8b',
-	func_alt     = '#ecdc8b',
-	str          = '#cd9169',
-	str_alt      = '#cdaf69',
+		-- Highlights
+		type         = '#81aece',
+		type_alt     = '#7ec9b1',
+		reserved     = '#cda2e3',
+		reserved_alt = '#af91d5',
+		id           = '#ebe3cb',
+		id_alt       = '#e6b995',
+		literal      = '#c4dc9b',
+		literal_alt  = '#8ac87d',
+		func         = '#ecdc8b',
+		func_alt     = '#ecdc8b',
+		str          = '#cd9169',
+		str_alt      = '#cdaf69',
 
-	-- Diagnostic
-	error = '#d83e33',
-	warn  = '#f2ba41',
-	hint  = '#cda1ac',
-	info  = '#c8889f',
+		-- Diagnostic
+		error = '#d83e33',
+		warn  = '#f2ba41',
+		hint  = '#cda1ac',
+		info  = '#c8889f',
+	},
+	term = {
+		-- Main colors
+		bg        = nil,
+		bg_alt    = 'black',
+		bg_br     = 'black',
+		bg_br_alt = 'black',
+		fg        = nil,
+		fg_alt    = 'white',
+		fg_br     = 'white',
+		fg_br_alt = 'white',
+
+		-- Highlights
+		type         = 'blue',
+		type_alt     = 'blue',
+		reserved     = 'magenta',
+		reserved_alt = 'magenta',
+		id           = 'white',
+		id_alt       = 'white',
+		literal      = 'green',
+		literal_alt  = 'green',
+		func         = 'yellow',
+		func_alt     = 'yellow',
+		str          = 'yellow',
+		str_alt      = 'yellow',
+
+		-- Diagnostic
+		error = 'red',
+		warn  = 'yellow',
+		hint  = 'magenta',
+		info  = 'magenta',
+	},
 }
 
 return C
--- lua/nvim-theme/theme.lua
+++ lua/nvim-theme/theme.lua
@@ -1,6 +1,5 @@
 local M = {}
 
-local hi = require 'nvim-theme.hi'.hi_set_pairs
 
 local def_colors = require 'nvim-theme.colors'
 
@@ -27,7 +26,8 @@ M.apply_theme = function(options)
 		end
 	end
 
-	local c = options.colors
+	local c = options.colors.regular
+	local tc = options.colors.term
 	local cursor_bg = options.bright_cursor_line and c.bg_alt or nil
 	local tr_bg     = nil
 	if not options.transparent then
@@ -36,85 +36,87 @@ M.apply_theme = function(options)
 
 	vim.cmd [[ colorscheme default ]]
 
+	local hi = require 'nvim-theme.hi'.hi_set_pairs
+
 	hi {
 		-- Core highlight groups
-		{'Normal', { bg = tr_bg, fg = c.fg }},
+		{'Normal', { bg = tr_bg, fg = c.fg, ctermfg = tc.fg }},
 		{'Italic', { italic = true }},
 		{'Bold', { bold = true }},
-		{'Visual', { fg = c.fg_br, bg = c.bg_br }},
-		{'LineNr', { fg = c.bg_br }},
-		{'SignColumn', { fg = c.fg_alt }},
-		{'IncSearch', { bg = c.literal, fg = c.bg_alt }},
-		{'Search', { bg = c.literal_alt, fg = c.bg_alt }},
+		{'Visual', { fg = c.fg_br, ctermfg = tc.fg_br, bg = c.bg_br, ctermbg = tc.bg_br }},
+		{'LineNr', { fg = c.bg_br, ctermfg = tc.bg_br }},
+		{'SignColumn', { fg = c.fg_alt, ctermfg = tc.fg_alt }},
+		{'IncSearch', { bg = c.literal, ctermbg = tc.literal, fg = c.bg_alt, ctermfg = tc.bg_alt }},
+		{'Search', { bg = c.literal_alt, ctermbg = tc.literal_alt, fg = c.bg_alt, ctermfg = tc.bg_alt }},
 		{'CursorLine', { bg = cursor_bg }},
-		{'CursorLineNr', { bg = cursor_bg, fg = c.bg_br_alt }},
+		{'CursorLineNr', { bg = cursor_bg, fg = c.bg_br_alt, ctermfg = tc.func }},
 		{'CursorLineSign', { bg = cursor_bg }},
-		{'MatchParen', { bg = c.bg_alt, fg = c.fg_br }},
-		{'Directory', { fg = c.type }},
-		{'SpecialKey', { bg = c.bg_alt, fg = c.type }},
-		{'EndOfBuffer', { fg = c.bg_br_alt }},
-		{'Pmenu', { bg = c.bg_alt, fg = c.fg_alt }},
-		{'PmenuSel', { bg = c.fg_alt, fg = c.bg_alt, bold = true }},
-		{'PmenuSBar', { bg = c.bg_br, fg = c.fg_br }},
-		{'PmenuThumb', { bg = c.fg_br, fg = c.bg_br }},
-		{'WildMenu', { bg = c.bg_alt, fg = c.fg_alt }},
-		{'ColorColumn', { bg = c.bg_br }},
-		{'Folded', { bg = tr_bg, fg = c.bg_br_alt, italic = not options.italic_comments }},
-		{'FoldColumn', { bg = c.tr_bg, fg = c.bg_br_alt }},
-		{'Title', { fg = c.type, bold = true }},
-		{'Question', { fg = c.fg }},
-		{'NonText', { fg = c.type, bold = true }},
-		{'MoreMsg', { fg = c.reserved_alt, bold = true }},
-		{'ErrorMsg', { fg = c.error }},
-		{'Todo', { fg = c.func, bold = true }},
-		{'WarningMsg', { fg = c.warn }},
-		{'Tabline', { bg = c.bg_alt }},
-		{'TablineSel', { bg = c.bg_br, bold = true }},
-		{'TablineFill', { bg = c.tr_bg }},
-		{'WinBar', { bg = c.bg_alt, fg = c.fg_alt }},
-		{'WinSeparator', { bg = c.bg, fg = c.bg }},
-		{'StatusLine', { bg = c.bg_alt, fg = c.fg_alt }},
-		{'StatusLineNC', { bg = c.bg, fg = c.bg_br_alt }},
-		{'DiffAdd', { fg = c.func }},
-		{'DiffText', { fg = c.reserved }},
-		{'DiffChange', { fg = c.reserved_alt }},
-		{'DiffDelete', { fg = c.bg_alt }},
+		{'MatchParen', { bg = c.bg_alt, ctermbg = tc.bg_alt, fg = c.fg_br, ctermfg = tc.fg_br }},
+		{'Directory', { fg = c.type, ctermfg = tc.type }},
+		{'SpecialKey', { bg = c.bg_alt, ctermbg = tc.bg_alt, fg = c.type, ctermfg = tc.type }},
+		{'EndOfBuffer', { fg = c.bg_br_alt, ctermfg = tc.bg_br_alt }},
+		{'Pmenu', { bg = c.bg_alt, ctermbg = tc.bg_alt, fg = c.fg_alt, ctermfg = tc.fg_alt }},
+		{'PmenuSel', { bg = c.fg_alt, ctermbg = tc.fg_alt, fg = c.bg_alt, ctermfg = tc.bg_alt, bold = true }},
+		{'PmenuSBar', { bg = c.bg_br, ctermbg = tc.bg_br, fg = c.fg_br, ctermfg = tc.fg_br }},
+		{'PmenuThumb', { bg = c.fg_br, ctermbg = tc.fg_br, fg = c.bg_br, ctermfg = tc.bg_br }},
+		{'WildMenu', { bg = c.bg_alt, ctermbg = tc.bg_alt, fg = c.fg_alt, ctermfg = tc.fg_alt }},
+		{'ColorColumn', { bg = c.bg_br, ctermbg = tc.bg_br }},
+		{'Folded', { bg = tr_bg, fg = c.bg_br_alt, ctermfg = tc.bg_br_alt, italic = not options.italic_comments }},
+		{'FoldColumn', { bg = c.tr_bg, ctermbg = tc.tr_bg, fg = c.bg_br_alt, ctermfg = tc.bg_br_alt }},
+		{'Title', { fg = c.type, ctermfg = tc.type, bold = true }},
+		{'Question', { fg = c.fg, ctermfg = tc.fg }},
+		{'NonText', { fg = c.type, ctermfg = tc.type, bold = true }},
+		{'MoreMsg', { fg = c.reserved_alt, ctermfg = tc.reserved_alt, bold = true }},
+		{'ErrorMsg', { fg = c.error, ctermfg = tc.error }},
+		{'Todo', { fg = c.func, ctermfg = tc.func, bold = true }},
+		{'WarningMsg', { fg = c.warn, ctermfg = tc.warn }},
+		{'Tabline', { bg = c.bg_alt, ctermbg = tc.bg_alt }},
+		{'TablineSel', { bg = c.bg_br, ctermbg = tc.bg_br, bold = true }},
+		{'TablineFill', { bg = c.tr_bg, ctermbg = tc.tr_bg }},
+		{'WinBar', { bg = c.bg_alt, ctermbg = tc.bg_alt, fg = c.fg_alt, ctermfg = tc.fg_alt }},
+		{'WinSeparator', { bg = c.bg, ctermbg = tc.bg, fg = c.bg, ctermfg = tc.bg }},
+		{'StatusLine', { bg = c.bg_alt, ctermbg = tc.bg_alt, fg = c.fg_alt, ctermfg = tc.fg_alt }},
+		{'StatusLineNC', { bg = c.bg, ctermbg = tc.bg, fg = c.bg_br_alt, ctermfg = tc.bg_br_alt }},
+		{'DiffAdd', { fg = c.func, ctermfg = tc.func }},
+		{'DiffText', { fg = c.reserved, ctermfg = tc.reserved }},
+		{'DiffChange', { fg = c.reserved_alt, ctermfg = tc.reserved_alt }},
+		{'DiffDelete', { fg = c.bg_alt, ctermfg = tc.bg_alt }},
 
 		-- Syntax
-		{'String', { fg = c.str }},
-		{'Comment', { fg = c.bg_br_alt, italic = options.italic_comments }},
+		{'String', { fg = c.str, ctermfg = tc.str }},
+		{'Comment', { fg = c.bg_br_alt, ctermfg = tc.type, italic = options.italic_comments }},
 		{'Special', nil },
 		{'Operator', nil },
-		{'Identifier', { fg = c.id }},
-		{'Constant', { fg = c.id_alt }},
-		{'Boolean', { fg = c.literal_alt }},
-		{'Number', { fg = c.literal }},
-		{'Function', { fg = c.func }},
-		{'Type', { fg = c.type }},
-		{'Keyword', { fg = c.reserved }},
-		{'Repeat', { fg = c.reserved }},
-		{'Conditional', { fg = c.reserved }},
-		{'PreProc', { fg = c.reserved_alt }},
-		{'Statement', { fg = c.fg_alt, bold = true }},
-		{'SpellBad', { fg = c.hint, undercurl = true }},
-		{'SpellCap', { fg = c.fg, undercurl = true }},
-		{'SpellLocal', { fg = c.fg, italic = true, undercurl = true }},
-		{'SpellRare', { fg = c.hint, undercurl = true }},
+		{'Identifier', { fg = c.id, ctermfg = tc.id }},
+		{'Constant', { fg = c.id_alt, ctermfg = tc.id_alt }},
+		{'Boolean', { fg = c.literal_alt, ctermfg = tc.literal_alt }},
+		{'Number', { fg = c.literal, ctermfg = tc.literal }},
+		{'Function', { fg = c.func, ctermfg = tc.func }},
+		{'Type', { fg = c.type, ctermfg = tc.type }},
+		{'Keyword', { fg = c.reserved, ctermfg = tc.reserved }},
+		{'Repeat', { fg = c.reserved, ctermfg = tc.reserved }},
+		{'Conditional', { fg = c.reserved, ctermfg = tc.reserved }},
+		{'PreProc', { fg = c.reserved_alt, ctermfg = tc.reserved_alt }},
+		{'Statement', { fg = c.fg_alt, ctermfg = tc.fg_alt, bold = true }},
+		{'SpellBad', { fg = c.hint, ctermfg = tc.hint, undercurl = true }},
+		{'SpellCap', { fg = c.fg, ctermfg = tc.fg, undercurl = true }},
+		{'SpellLocal', { fg = c.fg, ctermfg = tc.fg, italic = true, undercurl = true }},
+		{'SpellRare', { fg = c.hint, ctermfg = tc.hint, undercurl = true }},
 
 		-- Diagnostic
-		{'DiagnosticError', { fg = c.error }},
-		{'DiagnosticWarn', { fg = c.warn }},
-		{'DiagnosticHint', { fg = c.hint }},
-		{'DiagnosticInfo', { fg = c.info }},
-		{'DiagnosticUnderlineError', { fg = c.error, underline = true }},
-		{'DiagnosticUnderlineWarn', { fg = c.warn, underline = true }},
-		{'DiagnosticUnderlineHint', { fg = c.hint, underline = true }},
-		{'DiagnosticUnderlineInfo', { fg = c.info, underline = true }},
+		{'DiagnosticError', { fg = c.error, ctermfg = tc.error }},
+		{'DiagnosticWarn', { fg = c.warn, ctermfg = tc.warn }},
+		{'DiagnosticHint', { fg = c.hint, ctermfg = tc.hint }},
+		{'DiagnosticInfo', { fg = c.info, ctermfg = tc.info }},
+		{'DiagnosticUnderlineError', { fg = c.error, ctermfg = tc.error, underline = true }},
+		{'DiagnosticUnderlineWarn', { fg = c.warn, ctermfg = tc.warn, underline = true }},
+		{'DiagnosticUnderlineHint', { fg = c.hint, ctermfg = tc.hint, underline = true }},
+		{'DiagnosticUnderlineInfo', { fg = c.info, ctermfg = tc.info, underline = true }},
 
 		-- Treesitter
-		{'@constant.builtin', { fg = c.literal_alt }},
-		{'@namespace', { fg = c.type_alt }},
-		{'@text.literal', { fg = c.fg }},
+		{'@constant.builtin', { fg = c.literal_alt, ctermfg = tc.literal_alt }},
+		{'@namespace', { fg = c.type_alt, ctermfg = tc.type_alt }},
+		{'@text.literal', { fg = c.fg, ctermfg = tc.fg }},
 
 		-- Language specific
 		{'markdownCodeDelimiter', { link = 'Comment' }},
@@ -123,39 +125,39 @@ M.apply_theme = function(options)
 	-- Telescope
 	if options.plugin_integrations.telescope then
 		hi {
-			{'TelescopeNormal', { bg = c.bg }},
-			{'TelescopePromptBorder', { fg = c.fg_alt, bg = c.bg }},
-			{'TelescopeResultsBorder', { fg = c.fg_alt, bg = c.bg }},
-			{'TelescopePreviewBorder', { fg = c.fg_alt, bg = c.bg }},
-			{'TelescopeSelection', { bg = c.bg_alt }},
-			{'TelescopeMultiSelection', { bg = c.bg_alt, bold = true }},
-			{'TelescopeMatching', { fg = c.literal_alt, bold = true }},
-			{'TelescopePromptPrefix', { fg = c.fg_alt, bg = c.bg }},
+			{'TelescopeNormal', { bg = c.bg, ctermbg = tc.bg }},
+			{'TelescopePromptBorder', { fg = c.fg_alt, ctermfg = tc.fg_alt, bg = c.bg, ctermbg = tc.bg }},
+			{'TelescopeResultsBorder', { fg = c.fg_alt, ctermfg = tc.fg_alt, bg = c.bg, ctermbg = tc.bg }},
+			{'TelescopePreviewBorder', { fg = c.fg_alt, ctermfg = tc.fg_alt, bg = c.bg, ctermbg = tc.bg }},
+			{'TelescopeSelection', { bg = c.bg_alt, ctermbg = tc.bg_alt }},
+			{'TelescopeMultiSelection', { bg = c.bg_alt, ctermbg = tc.bg_alt, bold = true }},
+			{'TelescopeMatching', { fg = c.literal_alt, ctermfg = tc.literal_alt, bold = true }},
+			{'TelescopePromptPrefix', { fg = c.fg_alt, ctermfg = tc.fg_alt, bg = c.bg, ctermbg = tc.bg }},
 		}
 	end
 
 	-- mini.nvim
 	if options.plugin_integrations.mini_nvim then
 		hi {
-			{'MiniStatuslineFile', { bg = c.bg_br, fg = c.fg_alt }},
-			{'MiniStatuslineFileinfo', { bg = c.bg_br, fg = c.fg_alt }},
-			{'MiniStatuslineFilename', { bg = c.bg_alt, fg = c.fg }},
-			{'MiniStatuslineInactive', { bg = c.bg_alt, fg = c.bg_br_alt }},
-			{'MiniStatuslineDevinfo', { bg = c.bg_br, fg = c.fg_br }},
-			{'MiniStatuslineModeNormal', { bg = c.reserved, fg = c.bg, bold = true }},
-			{'MiniStatuslineModeInsert', { bg = c.type, fg = c.bg, bold = true }},
-			{'MiniStatuslineModeVisual', { bg = c.literal, fg = c.bg, bold = true }},
-			{'MiniStatuslineModeCommand', { bg = c.reserved_alt, fg = c.bg, bold = true }},
-			{'MiniStatuslineModeReplace', { bg = c.str_alt, fg = c.bg, bold = true }},
-			{'MiniStatuslineModeOther', { bg = c.bg_br_alt, fg = c.bg, bold = true }},
+			{'MiniStatuslineFile', { bg = c.bg_br, ctermbg = tc.bg_br, fg = c.fg_alt, ctermfg = tc.fg_alt }},
+			{'MiniStatuslineFileinfo', { bg = c.bg_br, ctermbg = tc.bg_br, fg = c.fg_alt, ctermfg = tc.fg_alt }},
+			{'MiniStatuslineFilename', { bg = c.bg_alt, ctermbg = tc.bg_alt, fg = c.fg, ctermfg = tc.fg }},
+			{'MiniStatuslineInactive', { bg = c.bg_alt, ctermbg = tc.bg_alt, fg = c.bg_br_alt, ctermfg = tc.bg_br_br_alt }},
+			{'MiniStatuslineDevinfo', { bg = c.bg_br, ctermbg = tc.bg_br, fg = c.fg_br, ctermfg = tc.fg_br }},
+			{'MiniStatuslineModeNormal', { bg = c.reserved, ctermbg = tc.reserved, fg = c.bg, ctermfg = tc.bg_br, bold = true }},
+			{'MiniStatuslineModeInsert', { bg = c.type, ctermbg = tc.type, fg = c.bg, ctermfg = tc.bg_br, bold = true }},
+			{'MiniStatuslineModeVisual', { bg = c.literal, ctermbg = tc.literal, fg = c.bg, ctermfg = tc.bg_br, bold = true }},
+			{'MiniStatuslineModeCommand', { bg = c.reserved_alt, ctermbg = tc.reserved_alt, fg = c.bg, ctermfg = tc.bg_br, bold = true }},
+			{'MiniStatuslineModeReplace', { bg = c.str_alt, ctermbg = tc.str_alt, fg = c.bg, ctermfg = tc.bg_br, bold = true }},
+			{'MiniStatuslineModeOther', { bg = c.bg_br_alt, ctermbg = tc.bg_br_alt, fg = c.bg, ctermfg = tc.bg_br, bold = true }},
 
-			{'MiniTablineHidden', { fg = c.fg_alt, bg = c.bg }},
-			{'MiniTablineModifiedHidden', { fg = c.fg_alt, bg = c.bg, italic = true }},
-			{'MiniTablineCurrent', { fg = c.fg_alt, bg = c.bg_alt, bold = true }},
-			{'MiniTablineModifiedCurrent', { fg = c.fg_alt, bg = c.bg_alt, bold = true, italic = true }},
+			{'MiniTablineHidden', { fg = c.fg_alt, ctermfg = tc.fg_alt, bg = c.bg, ctermbg = tc.bg }},
+			{'MiniTablineModifiedHidden', { fg = c.fg_alt, ctermfg = tc.fg_alt, bg = c.bg, ctermbg = tc.bg, italic = true }},
+			{'MiniTablineCurrent', { fg = c.fg_alt, ctermfg = tc.fg_alt, bg = c.bg_alt, ctermbg = tc.bg_alt, bold = true }},
+			{'MiniTablineModifiedCurrent', { fg = c.fg_alt, ctermfg = tc.fg_alt, bg = c.bg_alt, ctermbg = tc.bg_alt, bold = true, italic = true }},
 			{'MiniTablineVisible', { link = 'MiniTablineHidden' }},
 			{'MiniTablineModifiedVisible', { link = 'MiniTablineModifiedHidden' }},
-			{'MiniTablineTabpagesection', { bg = c.fg, fg = c.bg, bold = true}},
+			{'MiniTablineTabpagesection', { bg = c.fg, ctermbg = tc.fg, fg = c.bg, ctermfg = tc.bg, bold = true}},
 		}
 	end
 
